cdc3667103579a43adec7f84a4ff17016c757c83
Eduardo Valentim <eduardovalentim79@gmail.com>
2018-05-01 18:47:31 -0300

    Projeto Criado

---
+++ src/main/java/com/valentim/cursomvc/Cursomc4Application.java
@@ -0,0 +1,12 @@
+package com.valentim.cursomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc4Application.class, args);
+	}
+}
